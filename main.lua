bc5c751cb4e83d3337c109111bafc8f5c2b82957
Gimbaro <me@gimbaro.dev>
2025-09-02 09:53:35 +0200

    drawing tests with Dima

--- main.lua
+++ main.lua
@@ -1,14 +1,54 @@
 function love.load()
     math.randomseed(os.time())
+    love.window.setMode(1000, 768)
+
+    anim8 = require 'libraries/anim8/anim8'
+    cameraFile = require 'libraries/hump/camera'
+    wf = require 'libraries/windfield/windfield'
+    setupWorld()
+    camera = cameraFile()
 
     sprites = {
-        -- background = love.graphics.newImage("sprites/background.png"),
+        playerSheet = love.graphics.newImage('sprites/playerSheet.png'),
     }
+    local grid = imgToGrid(sprites.playerSheet, 4, 1)
+
+    animations = {
+        move = anim8.newAnimation(grid('1-4',1), 1),
+    }
+
+    require('player')
 
 end
 
 function love.update(dt)
+    world:update(dt)
+    
+    local px, py = player:getPosition()
+    camera:lookAt(px, love.graphics.getHeight()/2)
+
+    updatePlayer(dt)
+    animations.move:update(dt)
 end
 
 function love.draw()
+    world:draw()
+    local px, py = player:getPosition()
+    animations.move:draw(sprites.playerSheet, px, py, nil, 10)
+end
+
+function setupWorld()
+    world = wf.newWorld(0, 800, false)
+    world:setQueryDebugDrawing(true)
+    world:setGravity(0, 0) -- kekw
+end
+
+function imgToGrid(img, ncols, nrows)
+    return anim8.newGrid(math.floor(img:getWidth( )/ncols), math.floor(img:getHeight( )/nrows), img:getWidth(), img:getHeight())
+end
+
+function love.keypressed(key)
+    if key == "space" and player.body then
+        sprintPlayer()
+    end
 end
---
+++ player.lua
@@ -0,0 +1,63 @@
+playerStartX = 360
+playerStartY = 100
+
+world:addCollisionClass("player", {})
+player = world:newRectangleCollider(playerStartX, playerStartY, 40, 40, {
+    collision_class = "player"
+})
+
+player.speed = 200
+player.isSprinting = false
+
+function updatePlayer(dt)
+  movePlayer(dt)
+end
+
+function movePlayer(dt)
+  player.moveX, player.moveY = 0, 0
+  player.isSprinting = love.keyboard.isDown("lshift") or love.keyboard.isDown("rshift")
+  player.isMoving = false
+  local currentSpeed = player.isSprinting and player.sprintSpeed or player.speed
+
+  local px, py = player:getPosition()
+  if love.keyboard.isDown('right') then
+      player.isMoving = true
+      player.direction = 'right'
+      player:setX(px + player.speed*dt)
+  end
+  if love.keyboard.isDown('left') then
+      player.isMoving = true
+      player.direction = 'left'
+      player:setX(px - player.speed*dt)
+  end
+  if love.keyboard.isDown('up') then
+      player.isMoving = true
+      player.direction = 'up'
+      player:setY(py - player.speed*dt)
+  end
+  if love.keyboard.isDown('down') then
+      player.isMoving = true
+      player.direction = 'down'
+      player:setY(py + player.speed*dt)
+  end
+end
+
+function sprintPlayer()
+  -- change this and make it "diagonal"
+  if not player.sprinting then
+    player.sprinting = true
+    if player.direction == "right" then
+      player:applyLinearImpulse(1000, 0)
+    end
+    if player.direction == "left" then
+      player:applyLinearImpulse(-1000, 0)
+    end
+    if player.direction == "up" then
+      player:applyLinearImpulse(0, -1000)
+    end
+    if player.direction == "down" then
+      player:applyLinearImpulse(0, 1000)
+    end
+    player:applyLinearImpulse(0, 0)
+  end
+end
