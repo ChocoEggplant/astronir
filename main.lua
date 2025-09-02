847fb7da1e9134b68264faf312a819c7f2e79273
Gimbaro <me@gimbaro.dev>
2025-09-02 08:27:38 +0200

    #1 :tada: Basic Starting Setup

---
+++ main.lua
@@ -0,0 +1,14 @@
+function love.load()
+    math.randomseed(os.time())
+
+    sprites = {
+        -- background = love.graphics.newImage("sprites/background.png"),
+    }
+
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
