70986a7dc20fe960ef03c66cae486154f6c05fb3
MRdima98 <dimafrunza69@gmail.com>
2025-09-06 00:25:03 +0200

    True shit

--- base.lua
+++ base.lua
@@ -1,11 +1,11 @@
-
 local startX = 400
 local startY = 200
-local sprite = love.graphics.newImage('sprites/base.png')
+local sprite = love.graphics.newImage('sprites/Base.png')
 local grid = imgToGrid(sprite, 1, 1)
 local animations = {
   idle = anim8.newAnimation(grid('1-1', 1), 0.3),
 }
+
 
 world:addCollisionClass("base", {})
 base = world:newRectangleCollider(startX, startY, 120, 200, {
--- main.lua
+++ main.lua
@@ -1,12 +1,14 @@
 require('helpers')
 
 function love.conf(t)
-  t.console = true
+    t.console = true
 end
 
 function love.load()
+    WindowHeight = 768
+    WindowWidth = 1000
     math.randomseed(os.time())
-    love.window.setMode(1000, 768)
+    love.window.setMode(WindowWidth, WindowHeight)
     love.graphics.setDefaultFilter("nearest", "nearest")
 
     anim8 = require 'libraries/anim8/anim8'
@@ -17,7 +19,7 @@ function love.load()
 
     sprites = {
         playerSheet = love.graphics.newImage('sprites/player_sheet.png'),
-        
+
     }
 
     local sounds = {
@@ -30,27 +32,47 @@ function love.load()
     require('plants')
     require('base')
     require('player')
+    require('menu')
 end
 
 function love.update(dt)
     world:update(dt)
 
     local px, py = player:getPosition()
-    camera:lookAt(px, py)
+    if Menu then
+        if py > WindowHeight then
+            love.event.quit()
+        end
+
+        if player:enter('play') then
+            print('touched')
+            menu_component:destroy()
+            Exit:destroy()
+            Menu = false
+        end
+    else
+        camera:lookAt(px, py)
+        updatePlants(dt)
+    end
 
     updatePlayer(dt)
-    updatePlants(dt)
 end
 
 function love.draw()
     love.graphics.clear(0.25, 0.88, 0.82, 1)
     camera:attach()
-    world:draw()
 
     local px, py = player:getPosition()
-    drawBase(px, py)
+
+    world:draw()
+    if Menu then
+        drawMenu(px, py)
+    else
+        drawBase(px, py)
+        drawPlants(px, py)
+    end
+
     drawPlayer(px, py)
-    drawPlants(px, py)
     camera:detach()
 end
 
